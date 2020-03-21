3cadcde18ef42ad55b34fc46248907b553085ef0
Mainu <mainudev.co@gmail.com>
2020-03-21 15:18:58 +0600

    updated mobile responsive

--- js/main.js
+++ js/main.js
@@ -22,3 +22,34 @@ $("#myNavbar").onePageNav({
   filter: "",
   easing: "swing"
 });
+//topnav end
+
+//sidebar start
+function sidenavCloseNOpen() {
+  let showButton = document.getElementById("sidenavShowButton");
+  let closeButton = document.getElementById("sidenavCloseButton");
+  console.log(closeButton);
+  let overlay = document.querySelector(".overlay");
+  let sidenav = document.getElementById("mySideNav");
+  showButton.addEventListener("click", () => {
+    sidenav.style.transform = "translateX(-100%)";
+    sidenav.parentNode.className += " sidebar_active";
+  });
+  closeButton.addEventListener("click", sideNavClose);
+  overlay.addEventListener("click", sideNavClose);
+  function sideNavClose() {
+    sidenav.style.transform = "translateX(0)";
+    sidenav.parentNode.className = "my_sidebar";
+  }
+}
+sidenavCloseNOpen();
+//scroll
+$("#myNav2").onePageNav({
+  currentClass: "menu_active_2",
+  changeHash: false,
+  scrollSpeed: 750,
+  scrollThreshold: 0.5,
+  filter: "",
+  easing: "swing"
+});
+//sidebar end
